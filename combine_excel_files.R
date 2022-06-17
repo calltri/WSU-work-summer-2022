fd0c376be72a93dfbd1133ea0abeb2fb89743d8b
Madison W. Call <madison.call122@gmail.com>
2022-06-17 08:25:49 -0700

    Added some instructions

--- combine_excel_files.R
+++ combine_excel_files.R
@@ -1,13 +1,23 @@
-# Combines all the data from excel files in a folder to a given desination
-# 
+# Name: Madison Call
+# Date: 6/16/2022
+# Description: Combines all the data from excel files in a folder to a given destination
+# Keeps the column headers from the first file only
+# All file should be of the same type, no checks for this exist
+ 
 
 install.packages("readxl")
 library(readxl)
 
-all_files <- list.files("C:/Users/madis/Desktop/WSU work summer 2022/test_file_combine/test_files", all.files=FALSE,
-                        full.names=TRUE)
-print(all_files)
+#INSERT file locations here!
+# Put path to the folder that should hold the files to combine
+files_to_combine_location <- "C:/Users/madis/Desktop/WSU work summer 2022/test_file_combine/test_files"
+# Put file that should all files should be combined into
 combined_file <- "C:/Users/madis/Desktop/WSU work summer 2022/test_file_combine/combined_files.csv"
+
+all_files <- list.files(files_to_combine_location, all.files=FALSE,
+                        full.names=TRUE)
+
+print(all_files)
 
 first_run = TRUE
 for (file in all_files) {
