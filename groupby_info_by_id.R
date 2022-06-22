1bea4021a4ebca0777bf895c7635d9fc4f720510
Madison W. Call <madison.call122@gmail.com>
2022-06-22 13:20:29 -0700

    Made a few last minute edits and commenting cleanup

--- groupby_info_by_id.R
+++ groupby_info_by_id.R
@@ -2,8 +2,7 @@
 # Date: 6/16/2022
 # Description: Looks at an excel sheet, groups the data by ID, fills out any
 # empty locations with null values, converts to a table with the form 
-# 'ID, val1, val2,...' and writes to a csv. Repeats the process for all 
-# Columns other than ID
+# 'ID, val1, val2,...' and writes to a csv. 
 
 library(dplyr)
 library(readxl)
@@ -11,14 +10,14 @@ library(data.table)
 library(rlang)
 
 #INSERT file locations here! Make sure to use /
-# Put path to the folder that should hold the files to combine
-excel_data_file <- "C:/Users/madis/Documents/rstuff/WSU-work-summer-2022/excel_testing/groupby_testing/data.xlsx"
-# Put file that should all files should be combined into
+# Put path to the file with all relevant data
+data_file <- "C:/Users/madis/Documents/rstuff/WSU-work-summer-2022/excel_testing/groupby_testing/data.xlsx"
+# Desination route to end file
 # Make sure is file with .csv
 destination_file <- "C:/Users/madis/Documents/rstuff/WSU-work-summer-2022/excel_testing/groupby_testing/output.csv"
 
 
-data <- read_excel(excel_data_file)
+data <- read_excel(data_file)
 
 # INSERT the below line should be in the format:
 # split_data <- with(data, split(<The column you want to split with>, list(ID)))
@@ -32,13 +31,14 @@ for (row in split_data) {
   }
 }
 
+# Fill in 
 for (row_num in 1:length(split_data)) {
   row = unlist(split_data[row_num])
   while(TRUE) {
     if (length(row) == max_len) {
       break
     }
-    row <- c(row, "NULL")
+    row <- c(row, "")
   }
   split_data[row_num] <-list(row)
 }
@@ -46,5 +46,6 @@ for (row_num in 1:length(split_data)) {
 row_names <- names(split_data)
 split_data <- t(data.frame(split_data))
 row.names(split_data) <- row_names
+colnames(split_data) <- c(1:max_len)
 write.csv(split_data, destination_file)
 
