b9de38b75be9291282c3c73b7b0d14edcc50d3e2
Nicholas Call <nicholascall6@gmail.com>
2022-07-25 22:09:23 -0700

    added decipher bioc pkg to allow reading in newick files so we can get the same flexible visual api functionality as before with dendextend

--- phame/demo/show.r
+++ phame/demo/show.r
@@ -1,7 +1,16 @@
 
+BiocManager::install("DECIPHER")
+
 library(ape)
+library(dendextend)
+library(DECIPHER)
 
-help(read.tree)
 
-myTree <- read.tree(file = "./demo/phame_examples/ecoli/workdir/results/trees/ecoli_all.fasttree")
-plot(myTree)
+#myTree <- read.tree(file = "./demo/phame_examples/ecoli/workdir/results/trees/ecoli_all.fasttree")
+#plot(myTree)
+
+# this lets us read in Newick file formats into dendrograms
+a <- ReadDendrogram("./demo/phame_examples/ecoli/workdir/results/trees/ecoli_all.fasttree")
+a <- set(a, "labels_col", "blue") 
+plot(a)
+
